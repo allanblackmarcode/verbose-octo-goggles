e586a6fa3b7963ac800f98d9fc7b3c58d95e1c6d
Branden Lyle Schwartz <branden.schwartz005@gmail.com>
2022-02-28 11:12:38 -0500

    updated push with errors

--- octo-goggles/src/App.js
+++ octo-goggles/src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
+import Navbar from './Components/Navbar';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Components/Pages/Home';
+import Services from './Components/Pages/Services';
+import Products from './Components/Pages/Products';
+import ContactUs from './Components/Pages/ContactUs';
+import SignUp from './Components/Pages/SignUp';
+import Marketing from './Components/Pages/Marketing';
+import Consulting from './Components/Pages/Consulting';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' exact component={Home} />
+        <Route path='/services' exact component={Services} />
+        <Route path='/products' exact component={Products} />
+        <Route path='/contact-us' exact component={ContactUs} />
+        <Route path='/sign-up' exact component={SignUp} />
+        <Route path='/marketing' exact component={Marketing} />
+        <Route path='/consulting' exact component={Consulting} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ octo-goggles/src/Components/Pages/Services.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../../App.css';
+
+export default function Services() {
+  return (
+    <>
+        <h1 className='services'>SERVICES</h1>
+    </>
+  );
+}
